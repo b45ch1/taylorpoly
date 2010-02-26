ae3fcb98e5c293ffecaea0c1f400027ce2e93c36
Sebastian <b45ch1@shlp.(none)>
2010-02-26 10:16:27 +0100

    added tests for iadd and vectorized add

--- taylorpoly/taylorpoly/tests/test_utps.py
+++ taylorpoly/taylorpoly/tests/test_utps.py
@@ -6,6 +6,7 @@ from taylorpoly.utps import UTPS, mul, add
 class Test_Binary_Operators(TestCase):
     
     def test_add(self):
+        """ test z = x + y"""
         x = UTPS(numpy.array([1.,2.,3.]), P = 1, D = 3)
         y = UTPS(numpy.array([5.,7.,11.]), P = 1, D = 3)
         
@@ -13,8 +14,32 @@ class Test_Binary_Operators(TestCase):
         
         assert_array_almost_equal([6.,9.,14.], z.data)
     
-    
-    
+    def test_iadd(self):
+        """ test y += x"""
+        x = UTPS(numpy.array([1.,2.,3.]), P = 1, D = 3)
+        y = UTPS(numpy.array([5.,7.,11.]), P = 1, D = 3)
+        
+        y = add(y,x,y)
+        
+        assert_array_almost_equal([6.,9.,14.], y.data)
+        
+    def test_add_vectorized(self):
+        x = UTPS(numpy.array([1.,2.,3, 4.,6.]),P = 2, D = 3)
+        y = UTPS(numpy.array([5.,7.,11, 1.,2.]),P = 2, D = 3)
+        
+        z = add(x,y)
+        
+        assert_array_almost_equal([6.,9.,14., 5., 8.], z.data)
+        
+    def test_iadd_vectorized(self):
+        x = UTPS(numpy.array([1.,2.,3, 4.,6.]),P = 2, D = 3)
+        y = UTPS(numpy.array([5.,7.,11, 1.,2.]),P = 2, D = 3)
+        
+        y = add(y,x,y)
+        
+        assert_array_almost_equal([6.,9.,14., 5., 8.], y.data)
+        
+        
     
     def test_mul(self):
         x = UTPS(numpy.array([1.,2.,3.]), P = 1, D = 3)
