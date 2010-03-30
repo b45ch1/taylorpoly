b6d5c5e42d09d4bc8fdd6b486785fbf97402c45c
Sebastian <b45ch1@shlp.(none)>
2010-03-30 18:30:40 +0200

    solve now works for B = (N,1) arrays, probably will change memory layout to column major, this seems to be easier to handle

--- taylorpoly/test_experimental.py
+++ taylorpoly/test_experimental.py
@@ -3,16 +3,23 @@ import numpy
 from taylorpoly import *
 
 
-P,D,N = 3,3,3
-A = UTPM(numpy.random.rand((P*(D-1)+1)*N*N), shape = (N,N), P = 3)
-b = UTPM(numpy.random.rand((P*(D-1)+1)*N*1), shape = (N,1), P = 3)
+
+
+
+
+
+
+
+P,D,N,M = 3,3,3,1
+A = UTPM(numpy.random.rand((P*(D-1)+1)*N*N), shape = (N,N), P = P)
+b = UTPM(numpy.random.rand((P*(D-1)+1)*N*M), shape = (N,M), P = P)
 
 # A.coeff[0,1] *= 0
 # b.coeff[0,1] *= 0
 
 x = solve(A,b)
 
-assert_array_almost_equal(numpy.dot(A.coeff[0,0], x.coeff[0,0]), b.coeff[0,0])
-assert_array_almost_equal(x.coeff[0,1], numpy.linalg.solve(A.coeff[0,0], b.coeff[0,1] - numpy.dot(A.coeff[0,1],x.coeff[0,0])))
+# assert_array_almost_equal(numpy.dot(A.coeff[0,0], x.coeff[0,0]), b.coeff[0,0])
+# assert_array_almost_equal(x.coeff[0,1], numpy.linalg.solve(A.coeff[0,0], b.coeff[0,1] - numpy.dot(A.coeff[0,1],x.coeff[0,0])))
 assert_array_almost_equal(dot(A,x).data,b.data)
 
