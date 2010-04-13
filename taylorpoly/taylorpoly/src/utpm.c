2c11053996ace0cffd469935bfa15e09a31b0ccc
Sebastian <b45ch1@shlp.(none)>
2010-04-13 10:52:49 +0200

    added idiv

--- taylorpoly/taylorpoly/src/utpm.c
+++ taylorpoly/taylorpoly/src/utpm.c
@@ -17,6 +17,25 @@ inline int imul(int M, int N, double *y, int ldy, double *x, int ldx){
     return 0;
 }
 
+inline int idiv(int M, int N, double *y, int ldy, double *x, int ldx){
+    /* computes y /= x, where y and x are (M,N) arrays stored in column major format
+    with leading dimensions ldx and ldy.
+    */
+    
+    int n, m;
+    
+    for(n = 0; n < N; ++n){
+        for(m = 0; m < M; ++m){
+            (*y) /=  (*x);
+            ++y; ++x;
+        }
+        y += (ldy - M);
+        x += (ldx - M);
+    }
+
+    return 0;
+}
+
 inline int amul(int M, int N, double *x, int ldx, double *y, int ldy, double *z, int ldz){
     /* computes z = z + x * y, where y and x are (M,N) arrays stored in column major format
     with leading dimensions ldx and ldy.
