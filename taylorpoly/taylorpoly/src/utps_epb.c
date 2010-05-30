9191b1ae0fe3848c8e4558bd1b8790aa483cd21b
Sebastian Walter <sebastian.walter@gmail.com>
2010-05-30 12:55:43 +0200

    fixed a bug in epb_div and extended tests

--- taylorpoly/taylorpoly/src/utps_epb.c
+++ taylorpoly/taylorpoly/src/utps_epb.c
@@ -19,9 +19,11 @@ int utps_epb_mul(int P, int D, double *x, double *y, double *z, double *zbar, do
 }
 
 int utps_epb_div(int P, int D, double *x, double *y, double *z, double *zbar, double *xbar, double *ybar){
-    utps_amul(P, D, zbar, z, xbar );
-    utps_mul(P, D, z, z, z);
-    utps_amul(P, D, zbar, z, ybar );
-    return 0;
+    int err_code = 0;
+    err_code += utps_div(P, D, zbar, y, zbar );
+    err_code += utps_add(P, D, xbar, zbar, xbar);
+    err_code += utps_mul(P, D, zbar, z, zbar );
+    err_code += utps_sub(P, D, ybar, zbar, ybar );
+    return err_code;
 }
 
